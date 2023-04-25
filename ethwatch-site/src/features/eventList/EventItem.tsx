dc211e94f013edac00f6dff214bd7cea69f6b195
Henri Pihkala <henri@streamr.network>
2023-04-25 17:45:50 +0300

    site: updates to event styling

--- ethwatch-site/src/features/eventList/EventItem.tsx
+++ ethwatch-site/src/features/eventList/EventItem.tsx
@@ -15,11 +15,18 @@ export function EventItem({ id }: { id: string }) {
 		window.open(`${blockExplorerByChain[state.activeSubscription?.chain || 'ethereum']}${transactionHash}`, '_blank')
 	}
 
+	const progressBarStyle = `rgba(${event.accepted ? 0 : 46}, ${event.accepted ? 46 : 0}, 0, 0.5)`
+	const progressBarWidth = `${Math.round(100 * event.confirmations / state.seedNodes.length)}%`
+	const quorumLinePosition = `${Math.round(100 * event.requiredConfirmations / state.seedNodes.length)}%`
+
   return (
-		<li className={`${styles.eventItem} ${event.accepted ? styles.accepted : styles.notAccepted}`} key={id} onClick={() => openBlockExplorer(event.transactionHash)}>
-			<div className={styles.blockNumber}>#{event.block}</div>
-			<div className={styles.eventName}>{event.name} <img src={externalLinkImg} className={styles.externalLinkIcon} alt=""/></div>
-			<div className={styles.confirmations}>{Math.round(event.confirmations/event.requiredConfirmations*100)}% confirmed</div>
+		<li className={`${styles.eventItem} ${event.accepted ? styles.accepted : styles.notAccepted}`} 
+			style={{ backgroundImage: `linear-gradient(to right, ${progressBarStyle}, ${progressBarStyle} ${progressBarWidth}, rgba(0,0,0,0) ${progressBarWidth}, rgba(0,0,0,0))` }}
+			key={id} 
+			onClick={() => openBlockExplorer(event.transactionHash)}>
+			<div className={styles.quorumLine} style={{ left: quorumLinePosition}}></div>
+			<div className={styles.eventName}>{event.name} (block #{event.block})<img src={externalLinkImg} className={styles.externalLinkIcon} alt=""/></div>
+			<div className={styles.confirmations}>{`confirmations: ${event.confirmations}, required: ${event.requiredConfirmations}`}</div>
 			<table className={styles.args}>
 				<tbody>
 				{Object.keys(event.args).map((argName) => 
